69f836c04b1698066d7e3a8d4735b36d2d80b812
igorbrands <igorcantelmo@gmail.com>
2021-07-20 12:02:52 -0300

    feat: creates set local storage

--- main.js
+++ main.js
@@ -1,44 +1,13 @@
-let emailList;
-let emailOutput;
+const form = document.getElementById('form');
 
-function formatDate(date) {
-  const time = new Date(date);
-  return `${time.getDate()}/${time.getMonth()}/${time.getFullYear()}`;
-}
-
-function saveDate() {
-  localStorage.setItem('emails', JSON.stringify(emailList));
-}
-
-
-function onSubmit(e) {
-  const data = {};
-  const successMessage = 'Seu email foi cadastrado com sucesso ✅'
-
-  data.id = emailList.length;
-  data.email = e.target[0].value;
-  data.date = new Date();
-  
-
-  emailList.push(data);
-  saveDate(alert(successMessage));
-  
+form.addEventListener('submit', (e) => {
   e.preventDefault();
-}
 
+  let name = document.getElementById('name').value;
+  let mail = document.getElementById('email').value;
 
-window.addEventListener('load', () => {
+  localStorage.setItem('e-mail', mail);
+  localStorage.setItem('name', name);
 
-  emailOutput = document.getElementById('emails__output');
-  if (localStorage.getItem('emails')) {
-    emailList = JSON.parse(localStorage.getItem('emails'));
-  } else {
-    emailList = [];
-  }
-
-  document
-    .getElementById('form__email')
-    .addEventListener('submit', onSubmit);
-  
-    emailOutput.addEventListener('click', clickList);
+  document.createElement;
 });
